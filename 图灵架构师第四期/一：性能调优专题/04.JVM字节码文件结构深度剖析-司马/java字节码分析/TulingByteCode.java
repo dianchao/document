1c7c5ecded78b60b32f54a3e6f7d534209c90ffd
dianchao <947389057@qq.com>
2021-03-21 23:15:24 +0800

    资料

---
+++ 图灵架构师第四期/一：性能调优专题/04.JVM字节码文件结构深度剖析-司马/java字节码分析/TulingByteCode.java
@@ -0,0 +1,18 @@
+package com.tuling.smlz.jvm.classbyatecode;
+
+/**
+ * Created by smlz on 2019/11/5.
+ */
+public class TulingByteCode {
+
+    private String userName;
+
+    public String getUserName() {
+
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
